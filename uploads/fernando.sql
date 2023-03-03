70ce672923e2ef09d800ea237b2bd3f38e2dcd12
Fernando Osorio <mkdirfernando@gmail.com>
2023-03-03 10:34:05 -0300

    remover folder

--- uploads/fernando.sql
+++
@@ -1,6 +0,0 @@
-select p.nome, p.cpf , p.email , p.datanascimento , p.sexo, p.mae 
-, f.matricula , f.tipofuncionario_id , f.matricula 
-from pessoa p
-join funcionario f ON p.id = f.pessoafisica_id 
-where f.tipofuncionario_id = 1
-and f.matricula = '10233'
